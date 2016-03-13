0b2570aa5728f2eb7a765f652d3d3fc20d5655c7
Adam Serafini <adam_serafini@yahoo.co.uk>
2016-03-13 10:49:04 +0100

    No longer use date when archiving
    Just dump everything in the Backup folder.

--- tidyup.sh
+++ tidyup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-today="$(date +'%d%m%y')"
-destination=~/Backups/$today
+backupdir=~/Backups/
 
 for var in "$@"
   do
     source=~/$var
     # Ignore directories that don't exist.
     if [ -d "$source" ] ; then
+      destination=$backupdir$var
       mkdir -p $destination
-      rsync -arq $source $destination
+      rsync -arq $source $backupdir 
       rm -rf $source/*
     fi
   done
