fd92465e882d8056dea9314274f59d719b56e94f
adam <adam_serafini@yahoo.co.uk>
2016-03-30 18:07:17 +0200

    Comment the rm 'trick' to delete hidden files.

--- tidyup.sh
+++ tidyup.sh
@@ -14,9 +14,25 @@ for var in "$@"
     source=~/$var
     # Ignore directories that don't exist.
     if [ -d "$source" ] ; then
+
       destination=$backupdir$var/$today
       mkdir -p $destination
       rsync -arq $source/ $destination
+
+      # The following rm command removes:
+      #  - all subdirectories (and their contents)
+      #  - all hidden files
+      #  - all normal files
+
+      # This requires a 'trick' because normally removing .* results in the
+      # error that rm cannot remove '.' and '..'.
+
+      # It has three components that use wildcards:
+
+      #   1/ $source/..?* removes all dot-dot files and subdirs except ..
+      #   2/ $source/.[!.]* removes all dot files and subdirs except .
+      #   3/ $source/* removes all non dot files and subdirs
+
       rm -rf $source/..?* $source/.[!.]* $source/*
     fi
   done
