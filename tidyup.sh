9edbb5f42f9ebe36928c0d469fb4494954c6ddbb
adam <adam_serafini@yahoo.co.uk>
2016-04-01 14:56:55 +0200

    Use a main function.

--- tidyup.sh
+++ tidyup.sh
@@ -1,38 +1,46 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 0 ]] ; then
-  echo 'Usage: tidyup <dir1> <dir2> ... <dirN>'
-  echo 'Tidies ~/<dirN> to ~/Backups/<dirN>'
-  exit 0
-fi
+function tidy() {
 
-today="$(date +'%d%m%y')"
-backupdir=~/Backups/
+  source=$1
+  destination=$2
 
-for var in "$@"
+  # Only proceed if the source directory exists
+  if [ -d "$source" ] ; then
+    mkdir -p $destination
+    rsync -arq $source/ $destination
+
+    # The following rm command removes:
+    #  - all subdirectories (and their contents)
+    #  - all hidden files
+    #  - all normal files
+
+    # This requires a 'trick' because normally removing .* results in the
+    # error that rm cannot remove '.' and '..'.
+
+    # It has three components that use wildcards:
+
+    #   1/ $source/..?* removes all dot-dot files and subdirs except ..
+    #   2/ $source/.[!.]* removes all dot files and subdirs except .
+    #   3/ $source/* removes all non dot files and subdirs
+
+    rm -rf $source/..?* $source/.[!.]* $source/*
+  fi
+}
+
+function main() {
+  if [[ $# -eq 0 ]] ; then
+    echo 'Usage: tidyup <dir1> <dir2> ... <dirN>'
+    echo 'Tidies ~/<dirN> to ~/Backups/<dirN>'
+    exit 0
+  fi
+
+  # Today's date in ddmmyy format.
+  today="$(date +'%d%m%y')"
+  for var in "$@"
   do
-    source=~/$var
-    # Ignore directories that don't exist.
-    if [ -d "$source" ] ; then
-
-      destination=$backupdir$var/$today
-      mkdir -p $destination
-      rsync -arq $source/ $destination
-
-      # The following rm command removes:
-      #  - all subdirectories (and their contents)
-      #  - all hidden files
-      #  - all normal files
-
-      # This requires a 'trick' because normally removing .* results in the
-      # error that rm cannot remove '.' and '..'.
-
-      # It has three components that use wildcards:
-
-      #   1/ $source/..?* removes all dot-dot files and subdirs except ..
-      #   2/ $source/.[!.]* removes all dot files and subdirs except .
-      #   3/ $source/* removes all non dot files and subdirs
-
-      rm -rf $source/..?* $source/.[!.]* $source/*
-    fi
+    tidy ~/$var ~/Backups/$var/$today
   done
+}
+
+main "$@"
