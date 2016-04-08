5daccecd36564e57c07819e0c02452d4e8353f94
adam <adam_serafini@yahoo.co.uk>
2016-04-08 11:02:55 +0200

    Beginnings of a basic bash testing framework.
    1) Can "import" the test framework.
    2) Can run the test harnass.

---
+++ tests/bash-test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# (c) Adam Serafini 2016
+
+# My ad-hoc library for testing bash CLI projects.
+
+# Export the test directory:
+export DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+run() {
+  # Iterate through all files that match the test pattern.
+  for filename in $DIR/t[0-9]*-*.sh
+  do
+    $filename
+  done
+
+  exit 1
+}
+
+# Only 'run' the tests if this script is called directly:
+RUNNING="$(basename $0)"
+
+if [[ "$RUNNING" == "bash-test.sh" ]]
+then
+  run "$@"
+fi
