f04548d4b5d3724e95a18a13482c3a1a4c55d720
adam <adam_serafini@yahoo.co.uk>
2016-04-14 15:16:44 +0200

    Explantory comments in install.sh.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# Setup a target location in usr/local/bin for the script:
 dir=/usr/local/bin/
 filename=tidyup
 ext=.sh
 target=$dir$filename$ext
 
+# Copy the script from GitHub to the target location:
 src=https://raw.githubusercontent.com/adamserafini/tidyup/master/tidyup.sh
 curl $src -o $target
 chmod u+x $target
