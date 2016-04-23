0920215d39ba6e41b007456dfdac42e73e3ebf6f
adam <adam_serafini@yahoo.co.uk>
2016-04-23 15:21:46 +0200

    Implement removal of hidden files and directories.

--- emptydir.sh
+++ emptydir.sh
@@ -7,3 +7,16 @@ usage() {
   exit 1
 }
 
+emptyhidden() {
+  # Remove hidden files and subdirectories from a directory.
+
+  # This requires a 'trick' because normally removing .* results in the
+  # error that rm cannot remove '.' and '..'.
+
+  # It has two components that use wildcards:
+
+  #	 1/ $1/..?* removes all dot-dot files and subdirs except '..'
+  #	 2/ $1/.[!.]* removes all dot files and subdirs except '.'
+  rm -rf $1/..?* $1/.[!.]*
+}
+
