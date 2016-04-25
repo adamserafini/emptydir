bbb40054292b6bf55fafba486ba1d551573f2676
adam <adam_serafini@yahoo.co.uk>
2016-04-25 14:21:46 +0200

    Implement main function for emptydir script.

--- emptydir.sh
+++ emptydir.sh
@@ -25,3 +25,53 @@ empty() {
   rm -rf $1/*
 }
 
+main() {
+  # Store the cmd name in a variable (for error reporting)
+  cmdname=emptydir
+
+  # By default, do not empty the directory of hidden files or subdirectories
+  local remove_hidden=false
+
+  # OPTIND is the number of options found by getopts.
+  OPTIND=1
+
+  # Parse the options. The only valid option is -a (all) which empties the
+  # directory of hidden files and subdirectories.
+  while getopts ":a" opt; do
+    case $opt in
+      a)
+        remove_hidden=true
+        ;;
+      \?)
+        echo "$cmdname: illegal option: -- $OPTARG" >&2
+        usage
+        ;;
+    esac
+  done
+  # Remove options parsed by getopts.
+  shift $((OPTIND-1))
+
+  cd=emptydir
+  # Script returns 0 if all directories emptied, else 1.
+  retval=0
+  # Loop through the directory arguments.
+  for path in "$@"
+  do
+    if [[ -d $path ]]; then
+      empty $path
+      if [[ "$remove_hidden" = true ]]; then
+        emptyhidden $path
+      fi
+    elif [[ -f $path ]]; then
+      echo "$cmdname: $path: Not a directory" >&2
+      retval=1
+    else
+      echo "$cmdname: $path: No such file or directory" >&2
+      retval=1
+    fi
+  done
+
+  exit $retval
+}
+
+main "$@"
